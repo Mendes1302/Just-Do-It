58537bb12731f17a51ceec44d1325c2edac2f1ab
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2023-10-22 13:32:21 -0300

    Update counter_values.py

--- recursive/counter_values.py
+++ recursive/counter_values.py
@@ -2,12 +2,29 @@ from random import randint
 
 
 def counter(number_list, n) -> int:
-    if number_list == []: return 0
+    """
+    Recursively counts the number of elements in a list.
+
+    Args:
+    - number_list (list): The list of numbers to count.
+    - n (int): A parameter used in the recursive counting (default: 0).
+
+    Returns:
+    - int: The total count of elements in the list.
+    """
+    if number_list == []:
+        return 0
     number_list.pop(0)
-    return 1 + counter(number_list, n+1)
+    return 1 + counter(number_list, n + 1)
 
 
 def main() -> None:
+    """
+    Main function to generate a random list of numbers and count its size.
+
+    This function generates a random list of numbers, counts its size using the 'counter' function,
+    and checks if the result matches the actual size of the list.
+    """
     last_number = randint(3, 150)
     numbers_lists = [randint(0, last_number) for _ in range(last_number + 1)]
 
@@ -17,5 +34,5 @@ def main() -> None:
     print(f"[Size List] Check Values: {results == size_list}")
 
 
-
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+    main()
