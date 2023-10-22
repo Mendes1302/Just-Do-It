1977ff89b1ae9741b2fcf30ba255d5d52a3d90e3
Lucas Mendes Barbosa <lmlucasmendes1302@gmail.com>
2023-10-22 13:34:19 -0300

    Update binary_search_recursive.py

--- search/binary_search_recursive.py
+++ search/binary_search_recursive.py
@@ -1,6 +1,17 @@
 from random import randint
 
+
 def _guess_number(array, system_choice) -> list:
+    """
+    Recursively attempts to guess a number chosen by the system from a sorted list.
+
+    Args:
+    - array (list): The sorted list of numbers to choose from.
+    - system_choice (int): The number chosen by the system.
+
+    Returns:
+    - int: The guessed number if found, or None if not found.
+    """
     size_list = len(array)
     n = size_list // 2
     number = array[n]
@@ -13,7 +24,14 @@ def _guess_number(array, system_choice) -> list:
     return _guess_number(array, system_choice)
 
 
-def main()-> None:
+def main() -> None:
+    """
+    Main function to simulate a system choosing a number and attempting to guess it.
+
+    This function generates a sorted list of numbers and a number chosen by the system.
+    It then attempts to guess the system's chosen number using the '_guess_number' function.
+    The function displays the original list, its size, the system's choice, and the guessed number.
+    """
     last_number = randint(0, 50)
     numbers_lists = [i for i in range(last_number + 1)]
     size_list = len(numbers_lists)
@@ -26,4 +44,5 @@ def main()-> None:
     print(f"Number:   {number}")
 
 
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+    main()
